36ab238bd1b2a70da14c7ea8b03e980029fcba8d
Valentina Pich <169078817+Vaal-D4t4Sc@users.noreply.github.com>
2025-04-28 22:19:48 -0300

    Create Activ2-1.js

---
+++ Activ2-1.js
@@ -0,0 +1,11 @@
+const persona = { 
+  nombre: "Lucía", 
+  edad: 28, 
+  profesion: "Diseñadora",
+};
+console.log ("Lucía tiene 28 años y trabaja como Diseñadora")
+console.log (" ")
+
+const { nombre, edad, profesion } = persona;
+
+console.log(`${nombre} tiene ${edad} años y trabaja como ${profesion}.`)
