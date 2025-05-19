4b45f4317d965ac01a89b3ae9d15bdcde3e1c71a
Valentina Pich <169078817+Vaal-D4t4Sc@users.noreply.github.com>
2025-05-18 21:52:44 -0300

    Create App.jsx

---
+++ Evidencia 1/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import axios from 'axios';
+import {useEffect} from 'react'; // hook
+import {useState} from 'react'; //el hook useEffect permite ejecutar efectos secundarios en componentes funcionales
+
+//components
+import Card from "./components/card";
+import Footer from "./components/Footer";
+
+function App() {
+
+    const [products, setProducts] = useState ([]);
+
+    useEffect( ()=>{
+      axios.get(`https://dummyjson.com/products`).then((res)=>{setProducts(res.data.products)});
+  },[]);
+
+
+    return (
+      <>
+          <h1 className="text-center text-5xl font-bold font-sans">
+            Proyecto Integrador 2025 </h1>
+
+          <h1 className="text-center text-5xl font-bold font-sans">
+            Evidencia 1 y 2 (en progreso) </h1>
+
+          <h1 className="text-center text-5xl font-bold font-sans">
+            Alumna Pich Valentina</h1>
+          
+          <h2 className="text-left text-2xl font-bold font-sans"> Recetas sabrosas </h2>
+          <ul>
+            {products.map((p)=>(
+              <li>key={p.title}
+              {p.title},{p.price}</li>))}
+          </ul>
+
+          
+          <Footer />
+      </>
+  )
+}
+
+export default App;
